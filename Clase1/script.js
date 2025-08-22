6049940a041d9750a920a90edd07f872a9404dd4
CubiX54 <juancubides2435@gmail.com>
2025-08-22 12:15:42 -0500

    Ejercicio en clase 1

---
+++ Clase1/script.js
@@ -0,0 +1,15 @@
+const tareaInput = document.getElementById('demoTareaInput');
+const agregarBtn = document.getElementById('demoAgregarBtn');
+const listaTareas = document.getElementById('demoListaTareas');
+
+agregarBtn.addEventListener('click', function () {
+  const nuevaTareaTexto = tareaInput.value.trim();
+  if (nuevaTareaTexto === '') return;
+
+  const nuevaTareaLi = document.createElement('li');
+  nuevaTareaLi.textContent = nuevaTareaTexto;
+  listaTareas.appendChild(nuevaTareaLi);
+
+  tareaInput.value = '';
+  tareaInput.focus();
+});
